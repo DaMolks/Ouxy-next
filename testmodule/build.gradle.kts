0195902c754f1d013071c25997ee5be779f75124
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-14 23:11:14 +0100

    Fix: Convert test module to regular library module

--- testmodule/build.gradle.kts
+++ testmodule/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.dynamic-feature")
+    id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
     id("dagger.hilt.android.plugin")
