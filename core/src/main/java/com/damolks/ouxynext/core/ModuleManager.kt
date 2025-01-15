dcab750a70fad8e9a678859dd83ad1fd42c47c5e
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-15 11:34:20 +0100

    Fix: Move ModuleManager to core module

--- core/src/main/java/com/damolks/ouxynext/core/ModuleManager.kt
+++ core/src/main/java/com/damolks/ouxynext/core/ModuleManager.kt
@@ -12,7 +12,6 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Singleton
