495dcf769fdac7d5b2bcc82bf42fec9feec256c6
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-15 17:48:49 +0100

    Simplifie l'implémentation de EventBus et sa liaison

--- app/src/main/java/com/damolks/ouxynext/di/AppModule.kt
+++ app/src/main/java/com/damolks/ouxynext/di/AppModule.kt
@@ -1,13 +1,13 @@
 package com.damolks.ouxynext.di
 
+import com.damolks.ouxynext.core.EventBus
+import com.damolks.ouxynext.core.ModuleManager
+import com.damolks.ouxynext.impl.DefaultEventBus
+import com.damolks.ouxynext.impl.DefaultModuleManager
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import com.damolks.ouxynext.core.EventBus
-import com.damolks.ouxynext.core.ModuleManager
-import com.damolks.ouxynext.impl.DefaultModuleManager
-import com.damolks.ouxynext.impl.DefaultEventBus
 import javax.inject.Singleton
 
 @Module
