85f01320586fcfd91e3a3c50987d773e017e723d
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-15 17:42:48 +0100

    Ajout des interfaces core (EventBus, ModuleManager)

--- core/src/main/java/com/damolks/ouxynext/core/EventBus.kt
+++ core/src/main/java/com/damolks/ouxynext/core/EventBus.kt
@@ -1,21 +1,8 @@
 package com.damolks.ouxynext.core
 
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
-import javax.inject.Inject
-import javax.inject.Singleton
+import kotlinx.coroutines.flow.SharedFlow
 
-@Singleton
-class EventBus @Inject constructor() {
-    private val _events = MutableSharedFlow<Event>()
-    val events = _events.asSharedFlow()
-
-    suspend fun emit(event: Event) {
-        _events.emit(event)
-    }
-}
-
-sealed class Event {
-    data class ModuleEvent(val moduleId: String, val type: String, val data: Map<String, Any>? = null) : Event()
-    data class SystemEvent(val type: String, val data: Map<String, Any>? = null) : Event()
+interface EventBus {
+    val events: SharedFlow<Event>
+    suspend fun emit(event: Event)
 }
