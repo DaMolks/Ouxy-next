1cc8d01844b56d936edd11f7fdc00730eecac320
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-15 12:09:06 +0100

    Fix: Remove duplicate providers from AppModule

--- app/src/main/java/com/damolks/ouxynext/di/AppModule.kt
+++ app/src/main/java/com/damolks/ouxynext/di/AppModule.kt
@@ -3,9 +3,6 @@ package com.damolks.ouxynext.di
 import android.content.Context
 import com.damolks.ouxynext.core.EventBus
 import com.damolks.ouxynext.core.ModuleManager
-import com.damolks.ouxynext.core.data.OuxyDatabase
-import com.damolks.ouxynext.core.data.dao.EventLogDao
-import com.damolks.ouxynext.core.data.dao.ModuleStateDao
 import com.damolks.ouxynext.core.repository.EventLogRepository
 import com.damolks.ouxynext.core.repository.ModuleStateRepository
 import dagger.Module
@@ -18,36 +15,6 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
-
-    @Provides
-    @Singleton
-    fun provideDatabase(@ApplicationContext context: Context): OuxyDatabase {
-        return OuxyDatabase.getInstance(context)
-    }
-
-    @Provides
-    @Singleton
-    fun provideModuleStateDao(database: OuxyDatabase): ModuleStateDao {
-        return database.moduleStateDao()
-    }
-
-    @Provides
-    @Singleton
-    fun provideEventLogDao(database: OuxyDatabase): EventLogDao {
-        return database.eventLogDao()
-    }
-
-    @Provides
-    @Singleton
-    fun provideModuleStateRepository(moduleStateDao: ModuleStateDao): ModuleStateRepository {
-        return ModuleStateRepository(moduleStateDao)
-    }
-
-    @Provides
-    @Singleton
-    fun provideEventLogRepository(eventLogDao: EventLogDao): EventLogRepository {
-        return EventLogRepository(eventLogDao)
-    }
 
     @Provides
     @Singleton
