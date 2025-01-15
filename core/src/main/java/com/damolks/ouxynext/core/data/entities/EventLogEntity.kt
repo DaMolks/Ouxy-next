ffba12e418f6af615173d37b64d4108d44333e04
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-15 17:43:58 +0100

    Mise à jour de EventLogEntity pour inclure moduleId et eventType

--- core/src/main/java/com/damolks/ouxynext/core/data/entities/EventLogEntity.kt
+++ core/src/main/java/com/damolks/ouxynext/core/data/entities/EventLogEntity.kt
@@ -8,6 +8,7 @@ data class EventLogEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Long = 0,
     val timestamp: Long,
-    val type: String,
-    val data: String
+    val moduleId: String?,
+    val eventType: String,
+    val data: String?
 )
