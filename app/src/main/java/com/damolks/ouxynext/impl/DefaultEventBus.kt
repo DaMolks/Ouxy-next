a5c39ec788da787c1e1b7a474787b449e9bb3502
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-15 17:47:38 +0100

    Correction des implémentations pour gérer les properties correctement avec getter

--- app/src/main/java/com/damolks/ouxynext/impl/DefaultEventBus.kt
+++ app/src/main/java/com/damolks/ouxynext/impl/DefaultEventBus.kt
@@ -2,16 +2,18 @@ package com.damolks.ouxynext.impl
 
 import com.damolks.ouxynext.core.Event
 import com.damolks.ouxynext.core.EventBus
+import javax.inject.Inject
+import javax.inject.Singleton
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
-import javax.inject.Inject
-import javax.inject.Singleton
 
 @Singleton
 class DefaultEventBus @Inject constructor() : EventBus {
-    private val _events = MutableSharedFlow<Event>()
-    override val events: SharedFlow<Event> = _events.asSharedFlow()
+    private val _events = MutableSharedFlow<Event>(replay = 0, extraBufferCapacity = 64)
+
+    override val events: SharedFlow<Event>
+        get() = _events.asSharedFlow()
 
     override suspend fun emit(event: Event) {
         _events.emit(event)
--- app/src/main/java/com/damolks/ouxynext/impl/DefaultModuleManager.kt
+++ app/src/main/java/com/damolks/ouxynext/impl/DefaultModuleManager.kt
@@ -10,7 +10,8 @@ import javax.inject.Singleton
 @Singleton
 class DefaultModuleManager @Inject constructor() : ModuleManager {
     private val _installedModules = MutableLiveData<List<ModuleInfo>>(emptyList())
-    override val installedModules: LiveData<List<ModuleInfo>> = _installedModules
+    override val installedModules: LiveData<List<ModuleInfo>>
+        get() = _installedModules
 
     override fun loadModule(moduleId: String) {
         val modules = _installedModules.value?.map { 
