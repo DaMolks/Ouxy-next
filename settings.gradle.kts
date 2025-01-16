92237a0c55745a31f5063eab5f099d1ae9387fd0
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-16 12:24:58 +0100

    feat: initial core structure setup

---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+rootProject.name = "Ouxy-next"
+
+include(":app")
+
+// Core modules
+include(":core:events")
+include(":core:database")
+include(":core:security")
+include(":core:module")
+include(":core:api")
