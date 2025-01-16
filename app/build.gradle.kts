7e367687b3114168084f906fa719a70e14427109
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-16 16:17:32 +0100

    chore: upgrade to latest Gradle and AGP versions
    - Update Gradle to 8.2
    - Update AGP to 8.2.0
    - Enable modern features in app configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,16 +23,22 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 
     buildFeatures {
         viewBinding = true
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
     }
 }
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,11 @@
 buildscript {
-    val kotlinVersion = "1.9.0"
-    
     repositories {
         google()
         mavenCentral()
     }
-    
     dependencies {
-        classpath("com.android.tools.build:gradle:7.4.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
     }
 }
 
